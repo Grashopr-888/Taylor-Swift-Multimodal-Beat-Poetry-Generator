f46a40cda57fcf0a93fa24bf223b86cac0cec58d
YannaSmid <91134353+YannaSmid@users.noreply.github.com>
2023-10-19 21:41:15 +0200

    list of album 1-9

--- maincode.py
+++ maincode.py
@@ -6,4 +6,20 @@ import pandas as pd
 path = "Data"
 
 data_files = glob.glob(path +"\*.csv")
-print(data_files)
+#print(data_files)
+
+#lists
+all_dataframes = []
+lyric_list = []
+
+# open each file and add it to the dataframes list
+for file in data_files:
+    #print(file) #check if all files are in there
+    all_dataframes.append(pd.read_csv(file))
+    
+res = pd.concat(all_dataframes) #all dataframes merged into one
+
+#list with all the lyrics
+lyric_list = res["lyric"].tolist()
+
+
