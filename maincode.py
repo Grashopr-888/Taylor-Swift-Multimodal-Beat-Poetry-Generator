b2648732b91297d6dd398c06f1b99e68203b46c0
Nikki5386 <91133416+Nikki5386@users.noreply.github.com>
2023-10-19 23:11:51 +0200

    add vault lyrics to lyric list

--- maincode.py
+++ maincode.py
@@ -6,9 +6,11 @@ import nltk
 
 data_path = "Data"
 midnights_path = "Midnights"
+vault_path = "TheVault"
 
 data_files = glob.glob(data_path +"\*.csv")
 mid_txt_files = glob.glob(midnights_path + "\*.txt")
+vault_txt_files = glob.glob(vault_path + "\*.txt")
 
 #print(data_files)
 
@@ -25,13 +27,21 @@ res = pd.concat(all_dataframes) #all dataframes merged into one
 
 lyric_list = res["lyric"].tolist() # list with all the lyrics
 
-# open each file and add it to lyric_list
+# open each file and add it to lyric_list (midnights album)
 for txt_file in mid_txt_files:
     with open(txt_file, 'r', encoding='utf-8') as file:
         lines = file.read().splitlines()  # splitting by lines
         for line in lines:  # to add every string/line and not the entire list
             lyric_list.append(line)  # adding the midnights lyrics
 
-print(lyric_list)
+# open each file and add it to lyric_list (from the vault tracks)
+for txt_file in vault_txt_files:
+    with open(txt_file, 'r', encoding='utf-8') as file:
+        lines = file.read().splitlines()  # splitting by lines
+        for line in lines:  # to add every string/line and not the entire list
+            lyric_list.append(line)  # adding the midnights lyrics
+
+
+#print(lyric_list)
 
 
