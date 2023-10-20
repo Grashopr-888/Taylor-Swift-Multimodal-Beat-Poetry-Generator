67ced2fcc23ac39b450d535f0daa0d2dc77a3b34
Nikki5386 <91133416+Nikki5386@users.noreply.github.com>
2023-10-20 18:13:06 +0200

    replace contractions

--- maincode.py
+++ maincode.py
@@ -2,6 +2,7 @@ import numpy
 import csv
 import glob
 import pandas as pd
+import re
 
 # word package
 import nltk
@@ -59,16 +60,16 @@ print(df_all_lyrics)
 
 # ------ 2. PREPROCESSING ------
 
-#return the base of words
+# return the base of words
 def str_stemmer(s):
 
     return s.lower()
 
 def units_chars(s):
-	#replacing special characters with whitespaces
+	# replacing special characters with whitespaces
 	if isinstance(s, str):
 		s = s.replace("/"," ")
-		s = s.replace("\\"," ") #deletes single \ from string
+		s = s.replace("\\"," ") # deletes single \ from string
 		s = s.replace("//"," ")
 		s = s.replace("-"," ")
 		s = s.replace("#"," ")
@@ -80,8 +81,34 @@ def units_chars(s):
 		s = s.replace("$"," ")
 		s = s.replace("&"," ")
 		s = s.replace(","," ")
-		#s = s.replace("'"," ")
+        # s = s.replace("'"," ")
+		# s = s.replace('"',"")
 		return s
+
+
+def replace_contractions(s):
+    # replacing contractions
+    contractions_patterns = [
+        (r"won\'t", "will not"),
+        (r"can\'t", "cannot"),
+        (r"i\'m", "i am"),
+        (r"ain\'t", "is not"),
+        (r"(\w+)\'ll", "\g<1> will"),
+        (r"(\w+)n\'t", "\g<1> not"),
+        (r"(\w+)\'ve", "\g<1> have"),
+        (r"(he|she|it|that|there|who)\'s", "\g<1> is"), # distinguishing between contractions and possessives
+        (r"(\w+)\'re", "\g<1> are"),
+        (r"(\w+)\'d", "\g<1> would")
+    ]
+    
+    for pattern, replacement in contractions_patterns:
+        s = re.sub(pattern, replacement, s)
+    
+    return s
+
+# replace the contractions within the lyrics 
+df_all_lyrics['Lyrics'] = df_all_lyrics['Lyrics'].apply(replace_contractions)
+
      
 #apply stemmer to dataframe for columns: search_term, product_title, and product_description
 df_all_lyrics['Lyrics'] = df_all_lyrics['Lyrics'].map(lambda x:str_stemmer(x))
